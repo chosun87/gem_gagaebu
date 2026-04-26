4458123b11f99bff4bb31ef43cacc97d5f7a32c2
chosun87 <chosun87@gmail.com>
2026-04-26 20:38:24 +0900

    달력 상단 집계

--- src/pages/Ledger/Calendar.jsx
+++ src/pages/Ledger/Calendar.jsx
@@ -42,24 +42,52 @@ export default function Calendar() {
       const dateStr = dayjs(item.gDate).format('YYYY-MM-DD');
       if (!summary[dateStr]) {
         summary[dateStr] = {
-          income0: 0, expense0: 0, transfer0: 0,
-          income1: 0, expense1: 0, transfer1: 0,
+          income0: 0, expense0: 0, transfer0: 0, length0: 0,
+          income1: 0, expense1: 0, transfer1: 0, length1: 0
         };
       }
 
       const amount = Number(item.gAmount) || 0;
       if (!item.gExecuted) {
-        if (item.gType === '수입') summary[dateStr].income0 += amount;
-        else if (item.gType === '지출') summary[dateStr].expense0 += amount;
-        else if (item.gType === '이체') summary[dateStr].transfer0 += amount;
+        if (item.gType === '수입') { summary[dateStr].income0 += amount; ++summary[dateStr].length0; }
+        else if (item.gType === '지출') { summary[dateStr].expense0 += amount; ++summary[dateStr].length0; }
+        else if (item.gType === '이체') { summary[dateStr].transfer0 += amount; ++summary[dateStr].length0; }
       } else {
-        if (item.gType === '수입') summary[dateStr].income1 += amount;
-        else if (item.gType === '지출') summary[dateStr].expense1 += amount;
-        else if (item.gType === '이체') summary[dateStr].transfer1 += amount;
+        if (item.gType === '수입') { summary[dateStr].income1 += amount; ++summary[dateStr].length1; }
+        else if (item.gType === '지출') { summary[dateStr].expense1 += amount; ++summary[dateStr].length1; }
+        else if (item.gType === '이체') { summary[dateStr].transfer1 += amount; ++summary[dateStr].length1; }
       }
     });
     return summary;
   }, [yearData]);
+
+  // 월별 합계 계산
+  const monthTotal = useMemo(() => {
+    const total = {
+      income0: 0, expense0: 0, transfer0: 0,
+      income1: 0, expense1: 0, transfer1: 0,
+      incomeA: 0, expenseA: 0, transferA: 0
+    };
+    if (!selectedDate || !dailySummary) return total;
+
+    const currentMonth = dayjs(selectedDate).format('YYYY-MM');
+    Object.keys(dailySummary).forEach(dateStr => {
+      if (dateStr.startsWith(currentMonth)) {
+        total.income0 += dailySummary[dateStr].income0
+        total.expense0 += dailySummary[dateStr].expense0;
+        total.transfer0 += dailySummary[dateStr].transfer0;
+
+        total.income1 += dailySummary[dateStr].income1;
+        total.expense1 += dailySummary[dateStr].expense1;
+        total.transfer1 += dailySummary[dateStr].transfer1;
+      }
+    });
+    total.incomeA = total.income0 + total.income1;
+    total.expenseA = total.expense0 + total.expense1;
+    total.transferA = total.transfer0 + total.transfer1;
+
+    return total;
+  }, [selectedDate, dailySummary]);
 
   /*
   // yearData에서 현재 선택된 달의 데이터만 필터링 (방어 코드 포함)
@@ -171,6 +199,7 @@ export default function Calendar() {
           {data?.income0 > 0 && <i className={`fa-solid fa-star gType-수입`}></i>}
           {data?.expense0 > 0 && <i className={`fa-solid fa-star gType-지출`}></i>}
           {data?.transfer0 > 0 && <i className={`fa-solid fa-star gType-이체`}></i>}
+          {data?.length0 > 0 && <span className="text-xs 실행전">{data?.length0}</span>}
         </div>
         <div className="daily-totals monospace text-xs">
           {(data?.income1 > 0 || data?.expense1 > 0 || data?.transfer1 > 0) &&
@@ -196,6 +225,45 @@ export default function Calendar() {
         onMonthChange={handleMonthChange}
         onViewDateChange={handleViewDateChange}
       />
+
+      <div className="monthly-summary monospace">
+        <table>
+          <colgroup>
+            <col width="15%" />
+            <col width="25%" />
+            <col width="25%" />
+            <col width="25%" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th className="text-left">구분</th>
+              <th className="text-right">실행 전</th>
+              <th className="text-right">실행 완료</th>
+              <th className="text-right">합계</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>수입</td>
+              <td className="text-right">{monthTotal?.income0.toLocaleString()}</td>
+              <td className="text-right">{monthTotal?.income1.toLocaleString()}</td>
+              <td className="text-right">{monthTotal?.incomeA.toLocaleString()}</td>
+            </tr>
+            <tr>
+              <td>지출</td>
+              <td className="text-right">{monthTotal?.expense0.toLocaleString()}</td>
+              <td className="text-right">{monthTotal?.expense1.toLocaleString()}</td>
+              <td className="text-right">{monthTotal?.expenseA.toLocaleString()}</td>
+            </tr>
+            <tr>
+              <td>이체</td>
+              <td className="text-right">{monthTotal?.transfer0.toLocaleString()}</td>
+              <td className="text-right">{monthTotal?.transfer1.toLocaleString()}</td>
+              <td className="text-right">{monthTotal?.transferA.toLocaleString()}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
       <div className="fc-swipe-wrapper" onTouchStart={onTouchStart} onTouchEnd={onTouchEnd}>
         <FullCalendar
