bdebbd2867da98e0fecd4f19e5aeb3fab1ba2c88
chosun87 <chosun87@gmail.com>
2026-05-01 21:11:13 +0900

    Updates

---
+++ assets/Assets-DXGZprF4.js
@@ -0,0 +1,1 @@
+import{d as e,t}from"./jsx-runtime-bQus-5Dq.js";var n=t();function r(){return(0,n.jsx)(e,{className:`app-page assets-page`,header:(0,n.jsx)(`h2`,{className:`page-title text-3xl`,children:`자산`}),children:(0,n.jsx)(`p`,{children:`자산 화면입니다.`})})}export{r as default};
