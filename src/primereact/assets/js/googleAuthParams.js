4d382f2a4bc0ee273058a16b4bb49c246038341a
chosun87 <chosun87@gmail.com>
2026-05-17 17:26:17 +0900

    보안 강화

--- src/primereact/assets/js/googleAuthParams.js
+++ src/primereact/assets/js/googleAuthParams.js
@@ -1,7 +1,6 @@
 export const GOOGLE_AUTH_PARAMS = {
   SRC: 'https://accounts.google.com/gsi/client',
-  CLIENT_ID:
-    '660525556283-dtpdooehas3u161nsstn2l4hufvndhpr.apps.googleusercontent.com',
+  CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
   SCOPES: 'https://www.googleapis.com/auth/spreadsheets',
   DISCOVERY_DOCS: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
   REDIRECT_URI: 'https://chosun87.github.io/gem_gagaebu',
